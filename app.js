11d9134989f7f74f9bfec57fce52088071ffb8f2
zahmis <meda829ratteru86@gmail.com>
2021-06-22 00:41:45 +0900

    fix directory

--- app.js
+++ app.js
@@ -21,4 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// app.get('/', require('./routes').index);
+
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// routes/index.js
+exports.index = function(req, res){
+  res.render('index', { name: 'John' });
+};
+
+
 module.exports = router;
