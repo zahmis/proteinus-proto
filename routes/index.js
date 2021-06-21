48687edff65e6cabc4e2c2b226c659b00f8f888f
zahmis <meda829ratteru86@gmail.com>
2021-06-22 00:45:19 +0900

    delete unuse code

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// routes/index.js
-exports.index = function(req, res){
-  res.render('index', { name: 'John' });
-};
-
-
 module.exports = router;
